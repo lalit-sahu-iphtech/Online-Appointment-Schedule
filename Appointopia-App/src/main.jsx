497ed1fd32b9c3236ed936ce04cb3d55d89bd2b7
Lalit Sahu <lalit.sahu@iphtechnologies.com>
2026-08-18 18:05:00 +0530

    add provider

--- Appointopia-App/src/main.jsx
+++ Appointopia-App/src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   <BrowserRouter>
-   <App />
-   </BrowserRouter>
-    
-    
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AppointmentProvider } from './context/AppointmentContext';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppointmentProvider>
+        <App />
+      </AppointmentProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
