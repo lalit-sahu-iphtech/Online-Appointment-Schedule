442eb22b16aedf0f362eb737db9fb6cb833e9e2e
Lalit Sahu <lalit.sahu@iphtechnologies.com>
2026-08-27 11:50:51 +0530

    add ToastProvider

--- Appointopia-App/src/main.jsx
+++ Appointopia-App/src/main.jsx
@@ -1,3 +1,4 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -5,15 +6,18 @@ import App from './App';
 import { AppointmentProvider } from './context/AppointmentContext';
 import './index.css';
 import { NotificationProvider } from './context/NotificationContext';
+import { ToastProvider } from './component/Toast'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <NotificationProvider>
-      <AppointmentProvider>
-        <App />
-      </AppointmentProvider>
-      </NotificationProvider>
+      <ToastProvider>  {/* ✅ Sabse outer */}
+        <NotificationProvider>
+          <AppointmentProvider>
+            <App />
+          </AppointmentProvider>
+        </NotificationProvider>
+      </ToastProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
